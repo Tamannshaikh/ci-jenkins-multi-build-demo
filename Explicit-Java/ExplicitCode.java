47ac139d48c889248d877f0de735d7bffd60e073
Tamannshaikh <136705521+Tamannshaikh@users.noreply.github.com>
2026-09-02 10:11:57 +0530

    Add Explicit Java code

---
+++ Explicit-Java/ExplicitCode.java
@@ -0,0 +1,23 @@
+public class ExplicitCode {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== EXPLICIT CODE EXECUTION =====");
+
+        int a = 50;
+        int b = 25;
+
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+
+        System.out.println("Addition = " + addition);
+        System.out.println("Subtraction = " + subtraction);
+        System.out.println("Multiplication = " + multiplication);
+
+        System.out.println("===== EXECUTION COMPLETED =====");
+    }
+}
